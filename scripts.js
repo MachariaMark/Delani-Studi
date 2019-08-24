96e25d5a108a3b21a25a6c0f3c4caf0fb0294463
MachariaMark <official1offmark@gmail.com>
2019-08-24 20:43:01 +0300

    Java Script content

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#des').click(function() {
+        $('#destext').toggle();
+    });
+    $('#dev').click(function() {
+        $('#devtext').toggle();
+    });
+    $('#pro').click(function() {
+        $('#protext').toggle();
+    });
+});
