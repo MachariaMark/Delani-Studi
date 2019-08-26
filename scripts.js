2c7ba2cf0f1b5c99d8b80e4d9ff0ae2fb3f10de8
MachariaMark <official1offmark@gmail.com>
2019-08-26 13:19:26 +0300

    touching up on the display and functionality

--- scripts.js
+++ scripts.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
     $('#des').click(function() {
+        $('#des').toggle();
         $('#destext').toggle();
     });
+});
+$(document).ready(function() {
+    $('#destext').click(function() {
+        $('#des').toggle();
+        $('#destext').toggle();
+    });
+});
+$(document).ready(function() {
     $('#dev').click(function() {
+        $('#dev').toggle();
         $('#devtext').toggle();
     });
+});
+$(document).ready(function() {
+    $('#devtext').click(function() {
+        $('#dev').toggle();
+        $('#devtext').toggle();
+    });
+});
+$(document).ready(function() {
     $('#pro').click(function() {
+        $('#pro').toggle();
+        $('#protext').toggle();
+    });
+});
+$(document).ready(function() {
+    $('#protext').click(function() {
+        $('#pro').toggle();
         $('#protext').toggle();
     });
 });
